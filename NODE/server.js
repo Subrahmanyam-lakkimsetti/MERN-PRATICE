205b7f00757fd92c0c96ba5b427b706c881232f6
LAKKIMSETTI N V S D Ramasubrahmanyam <119732527+Subrahmanyam-lakkimsetti@users.noreply.github.com>
2025-02-02 11:20:53 +0530

    updating files

--- NODE/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('node:http');
-
-const server = http.createServer((req, res) => {
-  res.end('hello');
-});
-
-server.listen(1102, () => {
-  console.log('----server srarted----');
-});
